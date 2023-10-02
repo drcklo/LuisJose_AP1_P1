abf52bd15fab466b8d2cd71bc254681d3d2fbd96
darcklito <luis_paulino5@ucne.edu.do>
2023-10-02 18:27:53 -0400

    Modelo creado con expresiones regulares y campos requeridos

---
+++ Models/Aportes.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuisJose_AP1_P1.Models
+{
+    public class Aportes
+    {
+
+        [Key]
+        public int AportesId { get; set; }
+
+        [RegularExpression("^a-zA-z$")]
+        [Required(ErrorMessage ="Este campo es requerido")]
+        public string Persona{ get; set; }
+
+        [RegularExpression("^a-zA-z$")]
+        [Required(ErrorMessage ="Este campo es requerido")]
+        public string Observacion { get; set; }
+
+        [RegularExpression("/d")]
+        [Required(ErrorMessage ="Este campo es requerido")]
+        public int Monto{ get; set; }
+    }
+}
