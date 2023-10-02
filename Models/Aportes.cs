fc7ace8efad6ebd8387ef2a757f3b3a8a999189c
darcklito <luis_paulino5@ucne.edu.do>
2023-10-02 19:32:48 -0400

    Error en aportes solucionado

--- Models/Aportes.cs
+++ Models/Aportes.cs
@@ -8,16 +8,17 @@ namespace LuisJose_AP1_P1.Models
         [Key]
         public int AportesId { get; set; }
 
-        [RegularExpression("^a-zA-z$")]
-        [Required(ErrorMessage ="Este campo es requerido")]
+        [RegularExpression(@"^a-zA-z$")]
+        [Required(ErrorMessage = "Este campo es requerido para hacer su aporte")]
         public string? Persona{ get; set; }
 
-        [RegularExpression("^a-zA-z$")]
-        [Required(ErrorMessage ="Este campo es requerido")]
+        [RegularExpression(@"^a-zA-z$")]
+        [Required(ErrorMessage = "Este campo es requerido para hacer su aporte")]
         public string? Observacion { get; set; }
 
-        [RegularExpression("/d")]
-        [Required(ErrorMessage ="Este campo es requerido")]
+        [RegularExpression(@"/d")]
+        [Required(ErrorMessage = "Este campo es requerido para hacer su aporte")]
         public int Monto{ get; set; }
     }
 }
+
