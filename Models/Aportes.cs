c7042d4e846947cb9085786f30f8df72da7344c2
darcklito <luis_paulino5@ucne.edu.do>
2023-10-02 18:56:27 -0400

    Modelo modificado

--- Models/Aportes.cs
+++ Models/Aportes.cs
@@ -10,11 +10,11 @@ namespace LuisJose_AP1_P1.Models
 
         [RegularExpression("^a-zA-z$")]
         [Required(ErrorMessage ="Este campo es requerido")]
-        public string Persona{ get; set; }
+        public string? Persona{ get; set; }
 
         [RegularExpression("^a-zA-z$")]
         [Required(ErrorMessage ="Este campo es requerido")]
-        public string Observacion { get; set; }
+        public string? Observacion { get; set; }
 
         [RegularExpression("/d")]
         [Required(ErrorMessage ="Este campo es requerido")]
